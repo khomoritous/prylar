4962b93010d746bb22b0beef8de12e351ebaff8f
Monde Mampe <monde.mampe@gmail.com>
2019-07-06 05:36:33 +0200

    börjar jobba med vyn och case 1.

--- Source Packages/se/moma/pryl/startup/Main.java
+++ Source Packages/se/moma/pryl/startup/Main.java
@@ -3,70 +3,20 @@ package se.moma.pryl.startup;
 import java.util.*;
 import se.moma.pryl.controller.Controller;
 import se.moma.pryl.integration.PersonSamling;
-import se.moma.pryl.model.Person;
 import se.moma.pryl.model.factory.PrylFactory;
+import se.moma.pryl.view.View;
 
 public class Main {
     
-    public static void main(String[] args)  {
-      List<Person> lista = null;
+  public static void main(String[] args)  {
+      Map<String, String> prylArgs = null;
+      Scanner scanner = null;
       PrylFactory prylFactory = null;
-      Map<String, String> prylArgs = new HashMap<>();
-      //PersonSamling personSamling = new PersonSamling(lista);
-      PersonSamling personSamling = new PersonSamling();
-        
-      Controller controller = new Controller(personSamling, prylFactory);
-	
-      Scanner sc = new Scanner(System.in);
+      View view = null;
+      view = new View(prylArgs, new Controller(new PersonSamling(), prylFactory), scanner);
       
-      //SkapaPerson
-      System.out.print("Namn på en person: ");
-      String namn = sc.nextLine();
-      prylArgs.put("namn", namn);
-      controller.registreraNyPerson(namn);
-        
-      //Skapa pryl
-      System.out.println("Vad heter personen som ska äga prylen? ");
-      String namnPåPerson = sc.nextLine();
-      System.out.print("Vad för sorts pryl ska skapas? ");
-     
-      String namnPåPryl = sc.nextLine();
-      if (namnPåPryl.equalsIgnoreCase("Smycke")) {
-        System.out.print("Vilket sorts smycke? ");
-	      String smycke  = sc.nextLine();
-	      System.out.print("Vilken metall är smycket gjord av? ");
-	      String metall = sc.nextLine();
-	      System.out.print("Antal ädelstenar? ");
-	      String ädelstenar = sc.nextLine();
-        prylArgs.put("smycke", smycke);
-        prylArgs.put("metall", metall);
-        prylArgs.put("ädelstenar", ädelstenar);
-        controller.skapaPrylTillPerson(namnPåPerson, prylArgs);
-      } else if (namnPåPryl.equalsIgnoreCase("Apparat")) {
-          System.out.print("Vilken sorts apparat? ");
-          String apparat = sc.nextLine();
-          System.out.print("Vad har apparaten för pris? ");
-          String pris = sc.nextLine();
-          System.out.print("Vad har apparaten för slitage? ");
-          String slitage = sc.nextLine();
-          prylArgs.put("apparat", apparat);
-          prylArgs.put("pris", pris);
-          prylArgs.put("slitage", slitage);
-          controller.skapaPrylTillPerson(namnPåPerson, prylArgs);
-        } else if (namnPåPryl.equalsIgnoreCase("aktie")) {
-            System.out.print("Vilken sort aktie? ");
-            String aktieNamn = sc.nextLine();
-            System.out.print("Antal aktier? ");
-            String antal = sc.nextLine();
-            System.out.println("Pris på aktie? ");
-            String pris = sc.nextLine();
-            prylArgs.put("namn", aktieNamn);
-            prylArgs.put("antal", antal);
-            prylArgs.put("pris", pris);
-            controller.skapaPrylTillPerson(namnPåPerson, prylArgs);
-        } else 
-            throw new IllegalArgumentException(namnPåPryl + " finns inte!");
-        }
+      view.vyn();
+  }
 }
 
 
--- Source Packages/se/moma/pryl/view/View.java
+++ Source Packages/se/moma/pryl/view/View.java
@@ -5,6 +5,15 @@
  */
 package se.moma.pryl.view;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import se.moma.pryl.controller.Controller;
+import se.moma.pryl.integration.PersonSamling;
+import se.moma.pryl.model.Person;
+import se.moma.pryl.model.factory.PrylFactory;
+
 /**
  *
  * @author monde
@@ -112,5 +121,94 @@ public class View {
 		    }
 		
 	    }
- */   
+ */  
+  
+     
+      PrylFactory prylFactory = null;
+      Map<String, String> prylArgs = null;
+      Controller controller = null;
+	    Scanner scanner = null;
+      
+      public View(Map<String, String> prylArgs, Controller controller, Scanner scanner) {
+        this.prylArgs = prylArgs;
+        this.prylArgs = new HashMap<>();
+        this.controller = controller;
+        this.scanner = scanner;
+        this.scanner = new Scanner(System.in);
+      }
+      
+      
+      public void valMeny() {
+        System.out.print("\n"+"1-Skapa person \n" /* + "2-Skapa pryl \n" + "3-Visa alla\n"+"4-Visa rikaste \n"+"5-Visa viss person \n"+"6-B�rskrasch \n"+"7- Avsluta"*/);
+		    System.out.println();
+      }
+	
+      
+     public void vyn() {
+       for(;;) {
+         valMeny();
+         int kommando = Integer.parseInt(scanner.nextLine());
+     
+         switch(kommando) {
+           case 1:
+             //SkapaPerson
+             System.out.print("Namn på en person: ");
+             String namn = scanner.nextLine();
+             controller.registreraNyPerson(namn);
+           break;
+           
+           default:
+             System.out.println("Felaktigt kommando! Försök igen!");
+             
+         }
+         
+                 
+       }
+     }
+      /*
+      prylArgs.put("namn", namn);
+        
+      //Skapa pryl
+      System.out.println("Vad heter personen som ska äga prylen? ");
+      String namnPåPerson = sc.nextLine();
+      System.out.print("Vad för sorts pryl ska skapas? ");
+     
+      String namnPåPryl = sc.nextLine();
+      if (namnPåPryl.equalsIgnoreCase("Smycke")) {
+        System.out.print("Vilket sorts smycke? ");
+	      String smycke  = sc.nextLine();
+	      System.out.print("Vilken metall är smycket gjord av? ");
+	      String metall = sc.nextLine();
+	      System.out.print("Antal ädelstenar? ");
+	      String ädelstenar = sc.nextLine();
+        prylArgs.put("smycke", smycke);
+        prylArgs.put("metall", metall);
+        prylArgs.put("ädelstenar", ädelstenar);
+        controller.skapaPrylTillPerson(namnPåPerson, prylArgs);
+      } else if (namnPåPryl.equalsIgnoreCase("Apparat")) {
+          System.out.print("Vilken sorts apparat? ");
+          String apparat = sc.nextLine();
+          System.out.print("Vad har apparaten för pris? ");
+          String pris = sc.nextLine();
+          System.out.print("Vad har apparaten för slitage? ");
+          String slitage = sc.nextLine();
+          prylArgs.put("apparat", apparat);
+          prylArgs.put("pris", pris);
+          prylArgs.put("slitage", slitage);
+          controller.skapaPrylTillPerson(namnPåPerson, prylArgs);
+        } else if (namnPåPryl.equalsIgnoreCase("aktie")) {
+            System.out.print("Vilken sort aktie? ");
+            String aktieNamn = sc.nextLine();
+            System.out.print("Antal aktier? ");
+            String antal = sc.nextLine();
+            System.out.println("Pris på aktie? ");
+            String pris = sc.nextLine();
+            prylArgs.put("namn", aktieNamn);
+            prylArgs.put("antal", antal);
+            prylArgs.put("pris", pris);
+            controller.skapaPrylTillPerson(namnPåPerson, prylArgs);
+        } else 
+            throw new IllegalArgumentException(namnPåPryl + " finns inte!");
+        }
+*/
 }
