626ea9d8ba17ace4eb88d0c10967fc72a40e869a
Monde Mampe <monde.mampe@gmail.com>
2020-07-02 04:07:30 +0200

    lägger till regex och textsträngvalidering för person.

--- Source Packages/se/moma/pryl/model/Person.java
+++ Source Packages/se/moma/pryl/model/Person.java
@@ -2,6 +2,8 @@ package se.moma.pryl.model;
 
 import se.moma.pryl.model.interfaces.Pryl;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -12,6 +14,9 @@ import java.util.*;
 public class Person {
     
     private final String namn;
+    private String patt = "^[A-ZÅÄÖa-zåäö]+$";
+    private Pattern p = Pattern.compile(patt);
+
     
     
     
@@ -23,6 +28,10 @@ public class Person {
     public Person(String namnPåPerson) {
       Objects.requireNonNull(namnPåPerson, "null");
       if (namnPåPerson.equals("")) throw new IllegalArgumentException("Måste ange ett namn!");
+      Matcher m = p.matcher(namnPåPerson);
+      if (!m.find()) throw new IllegalArgumentException(namnPåPerson + "...ett namn på en person måste bestå av bokstäver!");
+      if (namnPåPerson.length() > 10) throw new IllegalArgumentException("Ett namn får bestå av max tio bokstäver.");
+     
       this.namn = namnPåPerson;
     }
     
@@ -70,8 +79,7 @@ public class Person {
       Person person = new Person("Olle");
       System.out.println(person);
       Person person1 = new Person("Olle");
-      Person person2 = new Person("Pelle");
-      
+    
       assert person.equals(person1);
    }
 
